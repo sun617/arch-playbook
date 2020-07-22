fcdf2d1fee386383bee94deea7391be5ff6f39e7
Hongri Cui <choihongil@gmail.com>
2020-07-22 23:43:16 +0900

    show group and header to file list

--- roles/base/files/fish/exa.fish
+++ roles/base/files/fish/exa.fish
@@ -1,6 +1,6 @@
 if status --is-interactive
   alias ea "exa --all --long --git"
   alias ee exa
-  alias el "exa --long --git"
+  alias el "exa --long --header --group --git"
   alias et "exa --tree"
 end
