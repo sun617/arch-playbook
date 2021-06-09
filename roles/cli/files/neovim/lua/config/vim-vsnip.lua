1c1577f7272cb272c19273a23010d5ec90a292fa
Hongri Cui <hongri.cui@gmail.com>
2021-06-09 23:30:08 +0900

    Comment out some vsnip key maps

--- roles/cli/files/neovim/lua/config/vim-vsnip.lua
+++ roles/cli/files/neovim/lua/config/vim-vsnip.lua
@@ -8,7 +8,7 @@ vim.api.nvim_set_keymap("s", "<C-l>",   "vsnip#available(1)  ? '<Plug>(vsnip-exp
 
 -- Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
 -- See https://github.com/hrsh7th/vim-vsnip/pull/50
-vim.api.nvim_set_keymap("n", "s", "<Plug>(vsnip-select-text)", {})
-vim.api.nvim_set_keymap("x", "s", "<Plug>(vsnip-select-text)", {})
-vim.api.nvim_set_keymap("n", "S", "<Plug>(vsnip-cut-text)",    {})
-vim.api.nvim_set_keymap("x", "S", "<Plug>(vsnip-cut-text)",    {})
+-- vim.api.nvim_set_keymap("n", "s", "<Plug>(vsnip-select-text)", {})
+-- vim.api.nvim_set_keymap("x", "s", "<Plug>(vsnip-select-text)", {})
+-- vim.api.nvim_set_keymap("n", "S", "<Plug>(vsnip-cut-text)",    {})
+-- vim.api.nvim_set_keymap("x", "S", "<Plug>(vsnip-cut-text)",    {})
