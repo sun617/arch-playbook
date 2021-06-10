b1db3482461f29a881cde30f56a7cf435057f167
Hongri Cui <hongri.cui@gmail.com>
2021-06-10 18:25:45 +0900

    auto formatting for go files

--- roles/cli/files/neovim/lua/config/nvim-lspconfig.lua
+++ roles/cli/files/neovim/lua/config/nvim-lspconfig.lua
@@ -63,3 +63,6 @@ require'lspinstall'.post_install_hook = function ()
   setup_servers() -- reload installed servers
   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
+
+-- auto formatting
+vim.cmd([[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync()]])
