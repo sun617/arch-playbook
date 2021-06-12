53d34a8046749c70e068256e1d967f21f2e728f1
Hongri Cui <hongri.cui@gmail.com>
2021-06-12 20:56:41 +0900

    Lists builtin pickers

--- roles/cli/files/neovim/lua/config/telescope.lua
+++ roles/cli/files/neovim/lua/config/telescope.lua
@@ -13,18 +13,14 @@ require('telescope').setup{
     },
   }
 }
+
+-- Lists Built-in pickers
+vim.api.nvim_set_keymap("n", "<Leader>l",        "<Cmd>lua require('telescope.builtin').builtin()<CR>",            { noremap = true })
 -- File Pickers
 vim.api.nvim_set_keymap("n", "<Leader>f",        "<Cmd>lua require('telescope.builtin').git_files()<CR>",            { noremap = true })
 vim.api.nvim_set_keymap("n", "<Leader>g",        "<Cmd>lua require('telescope.builtin').live_grep()<CR>",            { noremap = true })
-
 -- Vim Pickers
 vim.api.nvim_set_keymap("n", "<Leader><Leader>", "<Cmd>lua require('telescope.builtin').buffers()<CR>",              { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>vc",       "<Cmd>lua require('telescope.builtin').commands()<CR>",             { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>vm",       "<Cmd>lua require('telescope.builtin').keymaps()<CR>",              { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>vq",       "<Cmd>lua require('telescope.builtin').quickfix()<CR>",             { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>vl",       "<Cmd>lua require('telescope.builtin').loclist()<CR>",              { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>vh",       "<Cmd>lua require('telescope.builtin').help_tags()<CR>",            { noremap = true })
-
 -- Neovim LSP Pickers
 vim.api.nvim_set_keymap("n", "gd",               "<Cmd>lua require('telescope.builtin').lsp_definitions()<CR>",      { noremap = true })
 vim.api.nvim_set_keymap("n", "gi",               "<Cmd>lua require('telescope.builtin').lsp_implementations()<CR>",  { noremap = true })
