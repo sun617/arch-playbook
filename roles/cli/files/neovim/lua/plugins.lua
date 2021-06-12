793c5ada28a002e31b03247f49e7304abed2c967
Hongri Cui <hongri.cui@gmail.com>
2021-06-12 23:52:38 +0900

    Config kommentary

--- roles/cli/files/neovim/lua/plugins.lua
+++ roles/cli/files/neovim/lua/plugins.lua
@@ -76,7 +76,14 @@ require('packer').startup(function()
   }
 
   -- kommentary
-  use 'b3nj5m1n/kommentary'
+  use {
+    'b3nj5m1n/kommentary',
+    config = function()
+      require('kommentary.config').configure_language("default", {
+        prefer_single_line_comments = true,
+      })
+    end
+  }
 
   -- vimagit
   use 'jreybert/vimagit'
