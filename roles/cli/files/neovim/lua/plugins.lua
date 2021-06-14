1a4ba1bd2582d745a2e7baae431c437bf18f14fd
Hongri Cui <hongri.cui@gmail.com>
2021-06-14 23:03:14 +0900

    Config kommentary

--- roles/cli/files/neovim/lua/plugins.lua
+++ roles/cli/files/neovim/lua/plugins.lua
@@ -83,6 +83,12 @@ require('packer').startup(function()
   use {
     'b3nj5m1n/kommentary',
     config = function()
+      -- https://github.com/b3nj5m1n/kommentary/issues/20
+      vim.g.kommentary_create_default_mappings = false
+      vim.api.nvim_set_keymap("n", "gcc", "<Plug>kommentary_line_default", {})
+      vim.api.nvim_set_keymap("n", "gc", "<Plug>kommentary_motion_default", {})
+      vim.api.nvim_set_keymap("v", "gc", "<Plug>kommentary_visual_default<C-c>", {})
+
       require('kommentary.config').configure_language("default", {
         prefer_single_line_comments = true,
       })
