c2c2d985434f0478889ecf4416a2a5049dc9fcf1
Hongri Cui <hongri.cui@gmail.com>
2021-06-14 22:48:56 +0900

    Config indent-blankline

--- roles/cli/files/neovim/lua/plugins.lua
+++ roles/cli/files/neovim/lua/plugins.lua
@@ -73,6 +73,10 @@ require('packer').startup(function()
   use {
     'lukas-reineke/indent-blankline.nvim',
     branch = 'lua',
+    config = function()
+      vim.g.indent_blankline_show_first_indent_level = false
+      vim.g.indent_blankline_show_trailing_blankline_indent = false
+    end
   }
 
   -- kommentary
