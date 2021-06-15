cb4ba2bcb222eb055deb623147c40ee1c8772441
Hongri Cui <hongri.cui@gmail.com>
2021-06-15 22:36:23 +0900

    Install treesitter parsers

--- roles/cli/files/neovim/lua/config/treesitter.lua
+++ roles/cli/files/neovim/lua/config/treesitter.lua
@@ -1,4 +1,31 @@
+local parsers = {
+  'bash',
+  'comment',
+  'css',
+  'dockerfile',
+  'fish',
+  'go',
+  'gomod',
+  'graphql',
+  'html',
+  'java',
+  'javascript',
+  'json',
+  'jsonc',
+  'lua',
+  'python',
+  'regex',
+  'ruby',
+  'rust',
+  'scss',
+  'toml',
+  'typescript',
+  'vue',
+  'yaml',
+};
+
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = parsers,
   highlight = {
     enable = true,
   },
