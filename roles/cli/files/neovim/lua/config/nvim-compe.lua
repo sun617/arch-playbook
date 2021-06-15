f19fe9d289dbf6c921538f865caf1e2918bfe1aa
Hongri Cui <hongri.cui@gmail.com>
2021-06-15 23:02:07 +0900

    format config

--- roles/cli/files/neovim/lua/config/nvim-compe.lua
+++ roles/cli/files/neovim/lua/config/nvim-compe.lua
@@ -65,16 +65,16 @@ _G.s_tab_complete = function()
   end
 end
 
-vim.api.nvim_set_keymap("i", "<Tab>",   "v:lua.tab_complete()",        {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>",   "v:lua.tab_complete()",        {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()",      {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()",      {expr = true})
+vim.api.nvim_set_keymap("i", "<Tab>",   "v:lua.tab_complete()",        { expr = true })
+vim.api.nvim_set_keymap("s", "<Tab>",   "v:lua.tab_complete()",        { expr = true })
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()",      { expr = true })
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()",      { expr = true })
 
--- inoremap <silent><expr> <C-Space> compe#complete()
--- vim.api.nvim_set_keymap("i", "<CR>",    "compe#confirm('<CR>')",       {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap("i", "<C-e>",   "compe#close('<C-e>')",        {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap("i", "<C-f>",   "compe#scroll({'delta': +4})", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap("i", "<C-d>",   "compe#scroll({'delta': -4})", {noremap = true, silent = true, expr = true})
+-- vim.api.nvim_set_keymap("i", "<C-Space>", "compe#complete()",            { noremap = true, silent = true, expr = true })
+-- vim.api.nvim_set_keymap("i", "<CR>",      "compe#confirm('<CR>')",       { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-e>",     "compe#close('<C-e>')",        { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-f>",     "compe#scroll({'delta': +4})", { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-d>",     "compe#scroll({'delta': -4})", { noremap = true, silent = true, expr = true })
 
 -- highlight
 -- vim.cmd[[highlight link CompeDocumentation NormalFloat]]
