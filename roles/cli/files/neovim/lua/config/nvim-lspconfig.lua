6b8980a1f41a367423105de2b89a64748222faac
Hongri Cui <hongri.cui@gmail.com>
2021-06-18 00:29:00 +0900

    Conditionally set autocmd

--- roles/cli/files/neovim/lua/config/nvim-lspconfig.lua
+++ roles/cli/files/neovim/lua/config/nvim-lspconfig.lua
@@ -30,6 +30,26 @@ local on_attach = function(client, bufnr)
   -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec([[
+    augroup lsp_document_highlight
+      autocmd! * <buffer>
+      autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    augroup END
+    ]], false)
+  end
+  -- auto formatting
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_exec([[
+    augroup lsp_formatting_sync
+      autocmd! * <buffer>
+      autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+    augroup END
+    ]], false)
+  end
 end
 
 -- use lsp snippet
@@ -47,6 +67,8 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
 -- map buffer local keybindings when the language server attaches
 local function setup_servers()
   require'lspinstall'.setup()
+
+  -- get all installed servers
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
     nvim_lsp[server].setup{
@@ -69,7 +91,3 @@ require'lspinstall'.post_install_hook = function ()
   setup_servers() -- reload installed servers
   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
 end
-
--- auto formatting
-vim.cmd([[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync()]])
-vim.cmd([[autocmd BufWritePre *.rb lua vim.lsp.buf.formatting_sync()]])
