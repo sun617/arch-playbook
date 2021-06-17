595832172e5f04c66543dac4a4b9cff74aee1bff
Hongri Cui <hongri.cui@gmail.com>
2021-06-17 21:11:05 +0900

    Config nvim-tree

--- roles/cli/files/neovim/lua/config/nvim-tree.lua
+++ roles/cli/files/neovim/lua/config/nvim-tree.lua
@@ -1,2 +1,5 @@
+vim.g.nvim_tree_side = 'right'
+vim.g.nvim_tree_width = 70
+
 vim.api.nvim_set_keymap("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>",   { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-h>", "<Cmd>NvimTreeFindFile<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<Cmd>NvimTreeFindFile<CR>", { noremap = true })
