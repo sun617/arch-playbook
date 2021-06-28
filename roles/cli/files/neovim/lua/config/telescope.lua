ad3b55699712ad217ceff751f3ef806330ae98a9
Hongri Cui <hongri.cui@gmail.com>
2021-06-28 21:58:36 +0900

    Map workspace diagnostics

--- roles/cli/files/neovim/lua/config/telescope.lua
+++ roles/cli/files/neovim/lua/config/telescope.lua
@@ -24,17 +24,17 @@ require('telescope').setup{
 }
 
 -- Lists Built-in pickers
-vim.api.nvim_set_keymap("n", "<Leader>l",        "<Cmd>lua require('telescope.builtin').builtin()<CR>",              { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>l",        "<Cmd>lua require('telescope.builtin').builtin()<CR>",                   { noremap = true })
 -- File Pickers
-vim.api.nvim_set_keymap("n", "<Leader>f",        "<Cmd>lua require('config.telescope').git_or_find_files()<CR>",     { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>g",        "<Cmd>lua require('telescope.builtin').live_grep()<CR>",            { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>f",        "<Cmd>lua require('config.telescope').git_or_find_files()<CR>",          { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>g",        "<Cmd>lua require('telescope.builtin').live_grep()<CR>",                 { noremap = true })
 -- Vim Pickers
-vim.api.nvim_set_keymap("n", "<Leader><Leader>", "<Cmd>lua require('telescope.builtin').buffers()<CR>",              { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader><Leader>", "<Cmd>lua require('telescope.builtin').buffers()<CR>",                   { noremap = true })
 -- Neovim LSP Pickers
-vim.api.nvim_set_keymap("n", "gd",               "<Cmd>lua require('telescope.builtin').lsp_definitions()<CR>",      { noremap = true })
-vim.api.nvim_set_keymap("n", "gi",               "<Cmd>lua require('telescope.builtin').lsp_implementations()<CR>",  { noremap = true })
-vim.api.nvim_set_keymap("n", "gr",               "<Cmd>lua require('telescope.builtin').lsp_references()<CR>",       { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>s",        "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", { noremap = true })
--- vim.api.nvim_set_keymap("n", "<Leader>ca",       "<Cmd>lua require('telescope.builtin').lsp_code_actions()<CR>",     { noremap = true })
+vim.api.nvim_set_keymap("n", "gd",               "<Cmd>lua require('telescope.builtin').lsp_definitions()<CR>",           { noremap = true })
+vim.api.nvim_set_keymap("n", "gi",               "<Cmd>lua require('telescope.builtin').lsp_implementations()<CR>",       { noremap = true })
+vim.api.nvim_set_keymap("n", "gr",               "<Cmd>lua require('telescope.builtin').lsp_references()<CR>",            { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>s",        "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>",      { noremap = true })
+vim.api.nvim_set_keymap("n", "<Leader>wd",       "<Cmd>lua require('telescope.builtin').lsp_workspace_diagnostics()<CR>", { noremap = true })
 
 return M
