80faa79b7d47f68eb93b829ac2371fc8871ffff8
Hongri Cui <hongri.cui@gmail.com>
2021-10-01 14:50:52 +0900

    Update nvim-tree settings

--- roles/cli/files/neovim/lua/config/nvim-tree.lua
+++ roles/cli/files/neovim/lua/config/nvim-tree.lua
@@ -1,5 +1,20 @@
-vim.g.nvim_tree_side = 'right'
-vim.g.nvim_tree_width = 70
+require'nvim-tree'.setup {
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`
+    width = 70,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'right',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = {}
+    }
+  }
+}
 
 vim.api.nvim_set_keymap("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>",   { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<Cmd>NvimTreeFindFile<CR>", { noremap = true })
