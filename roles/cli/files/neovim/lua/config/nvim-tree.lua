34cd8c830b8cb7f973e710d24a8c7d46054eb963
Hongri Cui <hongri.cui@gmail.com>
2021-10-07 19:20:58 +0900

    Update nvim-tree settings

--- roles/cli/files/neovim/lua/config/nvim-tree.lua
+++ roles/cli/files/neovim/lua/config/nvim-tree.lua
@@ -1,4 +1,17 @@
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 require'nvim-tree'.setup {
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable      = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = true,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+
   view = {
     -- width of the window, can be either a number (columns) or a string in `%`
     width = 70,
@@ -11,7 +24,9 @@ require'nvim-tree'.setup {
       -- if true, it will only use your list to set the mappings
       custom_only = false,
       -- list of mappings to set on the tree manually
-      list = {}
+      list = {
+        { key = "<C-s>",                        cb = tree_cb("split") },
+      }
     }
   }
 }
