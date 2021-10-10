b2f04d05b89a2f55f5a19abf03d7fdef52bec806
Hongri Cui <hongri.cui@gmail.com>
2021-10-10 22:52:19 +0900

    Update hop mappings

--- roles/cli/files/neovim/lua/config/hop.lua
+++ roles/cli/files/neovim/lua/config/hop.lua
@@ -2,5 +2,5 @@ require'hop'.setup {}
 
 vim.api.nvim_set_keymap('n', 'gw', "<Cmd>lua require'hop'.hint_words()<CR>", {})
 vim.api.nvim_set_keymap('n', 'gl', "<Cmd>lua require'hop'.hint_lines()<CR>", {})
-vim.api.nvim_set_keymap('n', 'gc', "<Cmd>lua require'hop'.hint_char1()<CR>", {})
-vim.api.nvim_set_keymap('n', 'gb', "<Cmd>lua require'hop'.hint_char2()<CR>", {})
+vim.api.nvim_set_keymap('n', 'gj', "<Cmd>lua require'hop'.hint_char1()<CR>", {})
+vim.api.nvim_set_keymap('n', 'gk', "<Cmd>lua require'hop'.hint_char2()<CR>", {})
