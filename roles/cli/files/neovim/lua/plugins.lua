4ecd7d420c9bc7f0457f0dabc9272e4b71142608
Hongri Cui <hongri.cui@gmail.com>
2021-10-11 22:18:38 +0900

    Update kommentary mappings

--- roles/cli/files/neovim/lua/plugins.lua
+++ roles/cli/files/neovim/lua/plugins.lua
@@ -92,7 +92,7 @@ require('packer').startup(function()
       vim.g.kommentary_create_default_mappings = false
       vim.api.nvim_set_keymap("n", "<Leader>cc", "<Plug>kommentary_line_default",   {})
       vim.api.nvim_set_keymap("n", "<Leader>c",  "<Plug>kommentary_motion_default", {})
-      vim.api.nvim_set_keymap("x", "<Leader>c",  "<Plug>kommentary_visual_default", {})
+      vim.api.nvim_set_keymap("x", "<Leader>c",  "<Plug>kommentary_visual_default<C-c>", {})
 
       require('kommentary.config').configure_language("default", {
         prefer_single_line_comments = true,
