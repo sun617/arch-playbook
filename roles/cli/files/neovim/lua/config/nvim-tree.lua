e35937cc1a5ae7c9d58287211b31a2d5dd70701a
Hongri Cui <hongri.cui@gmail.com>
2021-12-17 22:37:56 +0900

    Update nvim-tree key bindings

--- roles/cli/files/neovim/lua/config/nvim-tree.lua
+++ roles/cli/files/neovim/lua/config/nvim-tree.lua
@@ -32,4 +32,4 @@ require'nvim-tree'.setup {
 }
 
 vim.api.nvim_set_keymap("n", "<C-n>", "<Cmd>NvimTreeToggle<CR>",   { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<Cmd>NvimTreeFindFile<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-f>", "<Cmd>NvimTreeFindFile<CR>", { noremap = true })
