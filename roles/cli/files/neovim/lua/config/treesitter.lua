514bd1e4078d280caa2fc8e2cdcc85dd059e7ad9
Hongri Cui <hongri.cui@gmail.com>
2021-12-17 22:38:22 +0900

    Update treesitter configs

--- roles/cli/files/neovim/lua/config/treesitter.lua
+++ roles/cli/files/neovim/lua/config/treesitter.lua
@@ -13,6 +13,7 @@ local parsers = {
   'json',
   'jsonc',
   'lua',
+  'nix',
   'python',
   'regex',
   'ruby',
@@ -31,6 +32,15 @@ require'nvim-treesitter.configs'.setup {
   },
   indent = {
     enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+    },
   },
   -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
   context_commentstring = {
