2a4d3e159b1c35ced23fd84ab378fc7cc3813816
Hongri Cui <hongri.cui@gmail.com>
2022-02-22 15:43:54 +0900

    Update autopairs settings

--- roles/cli/files/neovim/lua/config/autopairs.lua
+++ roles/cli/files/neovim/lua/config/autopairs.lua
@@ -1,21 +1,31 @@
 local npairs = require('nvim-autopairs')
-npairs.setup()
+local Rule   = require'nvim-autopairs.rule'
 
--- skip it, if you use another global object
-_G.MUtils= {}
-
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-    else
-      return npairs.esc("<cr>")
-    end
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-
-vim.api.nvim_set_keymap('i' , '<CR>', 'v:lua.MUtils.completion_confirm()', { noremap = true , expr = true })
+npairs.setup({
+  fast_wrap = {},
+})
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']')
+}
