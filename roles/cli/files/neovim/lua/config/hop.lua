a796938ba2bba7742d2f5d512dd07c5473ce5614
Hongri Cui <hongri.cui@gmail.com>
2022-03-08 22:47:31 +0900

    Update hop key bindings

--- roles/cli/files/neovim/lua/config/hop.lua
+++ roles/cli/files/neovim/lua/config/hop.lua
@@ -2,5 +2,5 @@ require'hop'.setup {}
 
 vim.api.nvim_set_keymap('n', 'gw', "<Cmd>lua require'hop'.hint_words()<CR>", {})
 vim.api.nvim_set_keymap('n', 'gl', "<Cmd>lua require'hop'.hint_lines()<CR>", {})
-vim.api.nvim_set_keymap('n', 'gC', "<Cmd>lua require'hop'.hint_char1()<CR>", {})
-vim.api.nvim_set_keymap('n', 'gB', "<Cmd>lua require'hop'.hint_char2()<CR>", {})
+vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>", {})
+vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>", {})
